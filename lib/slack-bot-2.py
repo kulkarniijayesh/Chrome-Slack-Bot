e229c3f50102803f76cb76aa6aac526ade99a7ba
jayesh <jayesh.kulkarni@opcito.com>
2020-02-09 09:00:39 +0530

    adding api call to open youtube links

--- lib/slack-bot-2.py
+++ lib/slack-bot-2.py
@@ -1,6 +1,19 @@
 import os
 import slack
 
+def open_youtube_link(url, thread_ts):
+    print('PERFORMING ACTION - Youtube Redirect >> ', url)
+    web_client.chat_postMessage(
+        channel=channel_id,
+        text="opening youtube link -- {}".format(url),
+        thread_ts=thread_ts,
+        hidden=False
+        )
+
+def search_and_play_youtube_video(searchString):
+   print('PERFORMING ACTION - Youtube search and open >> ', searchString)
+
+
 @slack.RTMClient.run_on(event='message')
 def say_hello(**payload):
     data = payload['data']
@@ -29,21 +42,15 @@ def youtube_event(**payload):
     web_client = payload['web_client']
     channel_id = data['channel']
     message =  data.get('text', [])
-#    print((str(message)).lower())
     thread_ts = data['ts']
 
 
     if 'youtube' in (str(message)).lower():
         print('received >> ', message)
-        youtube_keyword, youtube_link = message.split()
-        print('PERFORMING ACTION - Youtube Redirect >> ', youtube_link)
+        if 'link' in (str(message)).lower():
 
-        web_client.chat_postMessage(
-                channel=channel_id,
-                text="opening youtube link -- {}".format(youtube_link),
-                thread_ts=thread_ts,
-                hidden=False
-                )
+            youtube_keyword, link_keyword, youtube_link = message.split()
+            openYoutubeLink(youtube_link)
 
 slack_token = "xoxb-942270408645-941809432772-Eow6s1dOzolTcOWCGEEHwUyg"
 rtm_client = slack.RTMClient(token=slack_token)
